a10e3f3138b4222bb6e4e58f33f4b8768f5cc293
meerrom <meerrom@gmail.com>
2024-10-23 22:03:12 +0200

    Add percolation and animation
    Heads up, there are still quite some TODO parts in the code.

--- simulation.py
+++ simulation.py
@@ -3,7 +3,9 @@ import noise
 import argparse
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap, BoundaryNorm
+import matplotlib.animation as animation
 
+# Take the arguments from the command line and return them
 def create_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--distribution", default='random', type=str,
@@ -14,76 +16,118 @@ def create_arg_parser():
                         help="Probability of trees catching fire.")
     parser.add_argument("-s", "--size", default=100, type=int,
                         help="The size of the square lattice.")
-    args = parser.parse_args()
+    args = parser.parse_args() 
     return args
 
+# Initialize the square lattice with trees based on the distribution type
 def init_square_lattice(n, p, type):
-    if type == 'random': # random tree distribution on the lattice
+    # random tree distribution on the lattice
+    if type == 'random': 
         lattice = np.random.choice([0, 1], size=(n, n), p=[1-p, p])
-    elif type == 'ordered': # vertical tree lines on the lattice
+
+    # TODO: implement variety with p on the ordered lattice
+    # (possibly add scatter so the trees can jump between lines)
+    # vertical tree lines on the lattice
+    elif type == 'ordered': 
         lattice = np.zeros((n, n))
         for i in range(n):
             for j in range(n):
                 if j % 2 == 0:
                     lattice[i, j] = 1
-    elif type == 'semi-random': # random tree distribution based on perlin noise
+
+    # TODO: implement variety with p on the semi-ordered lattice
+    # random tree distribution based on perlin noise
+    elif type == 'semi-random': 
         lattice = np.zeros((n, n))
         for i in range(n):
             for j in range(n):
                 result = noise.pnoise2(i/n, j/n, octaves=10, persistence=1, lacunarity=20.0)
                 lattice[i, j] = 0 if result < 0 else 1
+
     return lattice
 
-def show_grid(grid, title="Forest fire"):
-    plt.figure(figsize=(6, 6))
-    cmap = ListedColormap(['w', 'g', 'r'])
-    plt.imshow(grid, cmap=cmap, interpolation='none')
-    plt.title(title)
-    plt.axis('off')
-    plt.show()
+# Return a single image used in a matplotlib animation
+def return_image(lattice, title="Forest fire"):
+    cmap = ListedColormap(['w', 'g', 'r', 'k'])
+    norm = BoundaryNorm([0, 1, 2, 3, 4], cmap.N)
+    # plt.title(title) # TODO: this crashes the animation for some reason
+    # plt.axis('off')
+    im = plt.imshow(lattice, cmap=cmap, norm=norm, animated=True)
+    return [im]
 
+# Start the fire by setting a random tree on fire
 def random_arson(grid):
-    # get the indices of all the points where the value is 1
     indices = np.argwhere(grid == 1)
-    # pick a random index
     random_index = np.random.choice(indices.shape[0])
-    # get the random point
     random_point = indices[random_index]
-    # set the value of the random point to 2 in the grid
     grid[random_point[0], random_point[1]] = 2
 
-# find the neighbours of a point in the grid that are trees
-def find_neighbours(grid, starting_point):
+# Return true if site at i,j is inside of the lattice for the edges of the lattice
+def is_inside(lattice, i, j):
+    n = lattice.shape[0]
+    if i < n and j < n and i >= 0 and j >= 0:
+        return True
+    else:
+        return False
+
+# Check for all four neighbours if they're inside the lattice and are a tree
+def get_tree_neighbours(lattice, i, j):
     neighbours = []
-    # check if the point above is a tree
-    if grid[starting_point[0], starting_point[1] + 1] == 1:
-        neighbours.append(grid[starting_point[0], starting_point[1] + 1])
-    # check if the point above right is a tree
-    if grid[starting_point[0] + 1, starting_point[1] + 1] == 1:
-        neighbours.append(grid[starting_point[0] + 1, starting_point[1] + 1])
-    # check if the point right is a tree
-    if grid[starting_point[0] + 1, starting_point[1]] == 1:
-        neighbours.append(grid[starting_point[0] + 1, starting_point[1]])
-    # check if the point below right is a tree
-    if grid[starting_point[0] + 1, starting_point[1] - 1] == 1:
-        neighbours.append(grid[starting_point[0] + 1, starting_point[1] - 1])
-    # check if the point below is a tree
-    if grid[starting_point[0], starting_point[1] - 1] == 1:
-        neighbours.append(grid[starting_point[0], starting_point[1] - 1])
-    # check if the point below left is a tree
-    if grid[starting_point[0] - 1, starting_point[1] - 1] == 1:
-        neighbours.append(grid[starting_point[0] - 1, starting_point[1] - 1])
-    # check if the point left is a tree
-    if grid[starting_point[0] - 1, starting_point[1]] == 1:
-        neighbours.append(grid[starting_point[0] - 1, starting_point[1]])
-    # check if the point above left is a tree
-    if grid[starting_point[0] - 1, starting_point[1] + 1] == 1:
-        neighbours.append(grid[starting_point[0] - 1, starting_point[1] + 1])
-    return neighbours   
+    if is_inside(lattice, i-1, j) and lattice[i-1, j] == 1:
+        neighbours.append((i-1, j))
+    if is_inside(lattice, i, j-1) and lattice[i, j-1] == 1:
+        neighbours.append((i, j-1))
+    if is_inside(lattice, i, j+1) and lattice[i, j+1] == 1:
+        neighbours.append((i, j+1))
+    if is_inside(lattice, i+1, j) and lattice[i+1, j] == 1:
+        neighbours.append((i+1, j))
+    return neighbours
+
+# One step of the simulation. Return False either when no more trees are burning or when there is a spanning cluster.
+def step(lattice):
+    n = lattice.shape[0]
+    to_burn = set()
+    for i in range(n):
+        for j in range(n):
+            if lattice[i, j] == 2:
+                neighbours = get_tree_neighbours(lattice, i, j)
+                to_burn.update(neighbours)
+                lattice[i, j] = 3
+    for site in to_burn:
+        i, j = site
+        lattice[i, j] = 2
+    if len(to_burn) == 0 or is_spanning(lattice):
+        return False
+    else:
+        return True
+
+# Return true if the lattice contains a spanning cluster.
+def is_spanning(lattice):
+    if np.any(lattice[-1, :] == 3):
+        return True
+    else:
+        return False
+    
 
 if __name__ == "__main__":
+    images = []
+    fig = plt.figure(figsize=(6, 6))
     args = create_arg_parser()
+
     # Create a square lattice of size n x n with tree density p
-    lattice = init_square_lattice(args.size, args.treeprobability, args.distribution)
-    random_arson(lattice)
-    show_grid(lattice)
+    grid = init_square_lattice(args.size, args.treeprobability, args.distribution)
+    images.append(return_image(grid, 0))
+    random_arson(grid)
+    images.append(return_image(grid, 1))
+    nstep = 1
+
+    # TODO: the simulation is not working properly and stops early
+    while step(grid):
+        nstep += 1
+        images.append(return_image(grid, nstep))
+    ani = animation.ArtistAnimation(fig, images, interval=100, blit=True, repeat_delay=10000)
+
+    # TODO: the gif is not saved properly
+    ani.save(f'n={args.size}_p={int(100*args.treeprobability)}%_{args.distribution}.gif', dpi=80, writer='pillow') 
+
+    plt.show()
