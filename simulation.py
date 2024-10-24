7787107dce991f3f0a888b6d40f0e1c3605f79cf
meerrom <meerrom@gmail.com>
2024-10-24 14:48:12 +0200

    Add center start for the percolation

--- simulation.py
+++ simulation.py
@@ -22,6 +22,8 @@ def create_arg_parser():
                         help="Direction of the wind. Options are: N, S, E, W, NE, NW, SE, SW. If not specified, there is no wind.")
     parser.add_argument("-w", "--weather", default='normal', type=str,
                 help="The weather conditions. Options are: dry, normal, wet.")
+    parser.add_argument("-c", "--center", default=False, type=bool,
+            help="If the fire should start in the center of the lattice.")
     args = parser.parse_args() 
     return args
 
@@ -32,28 +34,28 @@ def get_wind_probabilities(wind_speed, wind_direction, weather):
         probabilties = [1, 1, 1, 1] # f.l.t.r. top, right, bottom, left
         return [x * weather for x in probabilties]
     elif wind_direction == 'N':
-        probabilties = [0.25, 0.7, 1, 0.7]
+        probabilties = [0.25, 1, 1, 1]
         return [x * weather for x in probabilties]
     elif wind_direction == 'S':
-        probabilties = [1, 0.7, 0.25, 0.7]
+        probabilties = [1, 1, 0.25, 1]
         return [x * weather for x in probabilties]
     elif wind_direction == 'E':
-        probabilties = [0.7, 0.25, 0.7, 1]
+        probabilties = [1, 0.25, 1, 1]
         return [x * weather for x in probabilties]
     elif wind_direction == 'W':
-        probabilties = [0.7, 1, 0.7, 0.25]
+        probabilties = [1, 1, 1, 0.25]
         return [x * weather for x in probabilties]
     elif wind_direction == 'NE':
-        probabilties = [0.1, 0.1, 0.3, 0.5]
+        probabilties = [0.5, 0.5, 1, 1]
         return [x * weather for x in probabilties]
     elif wind_direction == 'NW':
-        probabilties = [0.1, 0.5, 0.3, 0.1]
+        probabilties = [0.5, 1, 1, 0.5]
         return [x * weather for x in probabilties]
     elif wind_direction == 'SE':
-        probabilties = [0.5, 0.1, 0.1, 0.3]
+        probabilties = [1, 0.5, 0.5, 1]
         return [x * weather for x in probabilties]
     elif wind_direction == 'SW':
-        probabilties = [0.3, 0.1, 0.5, 0.1]
+        probabilties = [1, 1, 0.5, 0.5]
         return [x * weather for x in probabilties]
 
 # Initialize the square lattice with trees based on the distribution type
@@ -89,15 +91,44 @@ def return_image(lattice, title="Forest fire"):
     norm = BoundaryNorm([0, 1, 2, 3, 4], cmap.N)
     plt.title(title)
     plt.axis('off')
-    im = plt.imshow(lattice, cmap=cmap, norm=norm, animated=True)
+    im = plt.imshow(lattice, cmap=cmap, norm=norm, animated=True, origin='lower')
     return [im]
 
-# Start the fire by setting a random tree on fire
-def random_arson(grid):
-    indices = np.argwhere(grid == 1)
-    random_index = np.random.choice(indices.shape[0])
-    random_point = indices[random_index]
-    grid[random_point[0], random_point[1]] = 2
+def find_closest_tree(lattice):
+    n = lattice.shape[0]
+    center = n//2
+    min_distance = float('inf')
+    closest_tree = None
+    
+    # Loop through the lattice to find the closest tree (value == 1)
+    for i in range(n):
+        for j in range(n):
+            if lattice[i, j] == 1:
+                # Calculate Euclidean distance from the center
+                distance = np.sqrt((i - center) ** 2 + (j - center) ** 2)
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_tree = (i, j)
+    
+    return closest_tree
+
+# Start the fire by setting a random tree on fire based on the wind direction
+def random_arson(grid, center):
+    n = grid.shape[0]
+
+    if center:
+        if grid[n//2, n//2] == 1:
+            grid[n//2, n//2] = 2
+            return  
+        else:
+            closest_tree = find_closest_tree(grid)
+            grid[closest_tree[0], closest_tree[1]] = 2
+            return
+    else:
+        indices = np.argwhere(grid == 1)
+        random_index = np.random.choice(indices.shape[0])
+        random_point = indices[random_index]
+        grid[random_point[0], random_point[1]] = 2
 
 # Return true if site at i,j is inside of the lattice for the edges of the lattice
 def is_inside(lattice, i, j):
@@ -111,16 +142,16 @@ def is_inside(lattice, i, j):
 def get_tree_neighbours(lattice, i, j, wind_probabilities):
     neighbours = []
     if is_inside(lattice, i+1, j) and lattice[i+1, j] == 1: # top neighbour
-        if random.randint(0,100) < (wind_probabilities[0]*100):
+        if random.randint(0,100) <= (wind_probabilities[0]*100):
             neighbours.append((i+1, j))
     if is_inside(lattice, i, j+1) and lattice[i, j+1] == 1: # right neighbour
-        if random.randint(0,100) < (wind_probabilities[1]*100):
+        if random.randint(0,100) <= (wind_probabilities[1]*100):
             neighbours.append((i, j+1))
     if is_inside(lattice, i-1, j) and lattice[i-1, j] == 1: # bottom neigbour
-        if random.randint(0,100) < (wind_probabilities[2]*100):
+        if random.randint(0,100) <= (wind_probabilities[2]*100):
             neighbours.append((i-1, j)) 
     if is_inside(lattice, i, j-1) and lattice[i, j-1] == 1: # left neighbour
-        if random.randint(0,100) < (wind_probabilities[3]*100):
+        if random.randint(0,100) <= (wind_probabilities[3]*100):
             neighbours.append((i, j-1))
     return neighbours
 
@@ -170,7 +201,7 @@ if __name__ == "__main__":
     grid = init_square_lattice(args.size, args.treeprobability, args.distribution)
 
     images.append(return_image(grid))
-    random_arson(grid)
+    random_arson(grid, args.center)
     images.append(return_image(grid))
 
     # Get the probabilities of the neighbours catching fire based on the wind direction, wind speed and weather
@@ -180,10 +211,11 @@ if __name__ == "__main__":
     while step(grid, wind_probabilities):
         images.append(return_image(grid))
     ani = animation.ArtistAnimation(fig, images, interval=100, blit=True, repeat_delay=10000)
-    ani.save(f'gifs\{args.size}_{args.treeprobability}_{args.distribution}_{args.windspeed}_{args.winddirection}_{args.weather}.gif', dpi=80, writer='pillow') 
+    ani.save(f'gifs\{args.size}_{args.treeprobability}_{args.distribution}_{args.windspeed}_{args.winddirection}_{args.weather}_{args.center}.gif', dpi=80, writer='pillow') 
 
     # plt.show() # Uncomment to show the animation
     # TODO: calculate the percentage of trees burned down from the total number of trees
+    # amount of pixels in the lattice
     amount_of_trees = len(np.argwhere(grid == 1))
     amount_of_trees_burnt = len(np.argwhere(grid == 3))
     print(amount_of_trees, amount_of_trees_burnt)
