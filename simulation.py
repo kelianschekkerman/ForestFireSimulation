193e10dd6a656880b91f18e5d5a734b1e3fe2f14
meerrom <meerrom@gmail.com>
2024-10-24 10:43:50 +0200

    Add wind
    Add randomness for the neighbouring trees catching fire based on direction

--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import noise
 import argparse
+import random
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap, BoundaryNorm
 import matplotlib.animation as animation
@@ -23,6 +24,28 @@ def create_arg_parser():
                         help="Direction of the wind. Options are: N, S, E, W, NE, NW, SE, SW. If not specified, there is no wind.")
     args = parser.parse_args() 
     return args
+
+# Return the probabilities of neigbours catching fire based on the wind direction and wind speed
+# TODO: add a formula for the probabilities based on the wind speed and direction
+def get_wind_probabilities(wind_speed, wind_direction):
+    if wind_direction == None:
+        return [0.25, 0.25, 0.25, 0.25] # f.l.t.r. top, right, bottom, left
+    elif wind_direction == 'N':
+        return [0.5, 0.7, 1, 0.7]
+    elif wind_direction == 'S':
+        return [0.3, 0.3, 0.3, 0.1]
+    elif wind_direction == 'E':
+        return [0.3, 0.1, 0.3, 0.3]
+    elif wind_direction == 'W':
+        return [0.3, 0.3, 0.1, 0.3]
+    elif wind_direction == 'NE':
+        return [0.1, 0.1, 0.3, 0.5]
+    elif wind_direction == 'NW':
+        return [0.1, 0.5, 0.3, 0.1]
+    elif wind_direction == 'SE':
+        return [0.5, 0.1, 0.1, 0.3]
+    elif wind_direction == 'SW':
+        return [0.3, 0.1, 0.5, 0.1]
 
 # Initialize the square lattice with trees based on the distribution type
 def init_square_lattice(n, p, type):
@@ -55,8 +78,8 @@ def init_square_lattice(n, p, type):
 def return_image(lattice, title="Forest fire"):
     cmap = ListedColormap(['w', 'g', 'r', 'k'])
     norm = BoundaryNorm([0, 1, 2, 3, 4], cmap.N)
-    # plt.title(title) # TODO: this crashes the animation for some reason
-    # plt.axis('off')
+    plt.title(title)
+    plt.axis('off')
     im = plt.imshow(lattice, cmap=cmap, norm=norm, animated=True)
     return [im]
 
@@ -76,26 +99,30 @@ def is_inside(lattice, i, j):
         return False
 
 # Check for all four neighbours if they're inside the lattice and are a tree
-def get_tree_neighbours(lattice, i, j):
+def get_tree_neighbours(lattice, i, j, wind_probabilities):
     neighbours = []
-    if is_inside(lattice, i-1, j) and lattice[i-1, j] == 1:
-        neighbours.append((i-1, j))
-    if is_inside(lattice, i, j-1) and lattice[i, j-1] == 1:
-        neighbours.append((i, j-1))
-    if is_inside(lattice, i, j+1) and lattice[i, j+1] == 1:
-        neighbours.append((i, j+1))
-    if is_inside(lattice, i+1, j) and lattice[i+1, j] == 1:
-        neighbours.append((i+1, j))
+    if is_inside(lattice, i+1, j) and lattice[i+1, j] == 1: # top neighbour
+        if random.randint(0,100) < (wind_probabilities[0]*100):
+            neighbours.append((i+1, j))
+    if is_inside(lattice, i, j+1) and lattice[i, j+1] == 1: # right neighbour
+        if random.randint(0,100) < (wind_probabilities[1]*100):
+            neighbours.append((i, j+1))
+    if is_inside(lattice, i-1, j) and lattice[i-1, j] == 1: # bottom neigbour
+        if random.randint(0,100) < (wind_probabilities[2]*100):
+            neighbours.append((i-1, j)) 
+    if is_inside(lattice, i, j-1) and lattice[i, j-1] == 1: # left neighbour
+        if random.randint(0,100) < (wind_probabilities[3]*100):
+            neighbours.append((i, j-1))
     return neighbours
 
 # One step of the simulation. Return False either when no more trees are burning or when there is a spanning cluster.
-def step(lattice):
+def step(lattice, wind_probabilities):
     n = lattice.shape[0]
     to_burn = set()
     for i in range(n):
         for j in range(n):
             if lattice[i, j] == 2:
-                neighbours = get_tree_neighbours(lattice, i, j)
+                neighbours = get_tree_neighbours(lattice, i, j, wind_probabilities)
                 to_burn.update(neighbours)
                 lattice[i, j] = 3
     for site in to_burn:
@@ -108,7 +135,7 @@ def step(lattice):
 
 # Return true if the lattice contains a spanning cluster.
 def is_spanning(lattice):
-    if np.any(lattice[-1, :] == 3):
+    if np.any(lattice[-1, :] == 4):
         return True
     else:
         return False
@@ -120,17 +147,18 @@ if __name__ == "__main__":
     args = create_arg_parser()
 
     # Create a square lattice of size n x n with tree density p
-    grid = init_square_lattice(args.size, args.treeprobability, args.distribution, args.windspeed, args.winddirection)
+    grid = init_square_lattice(args.size, args.treeprobability, args.distribution)
     images.append(return_image(grid))
     random_arson(grid)
     images.append(return_image(grid))
 
-    # TODO: the simulation is not working properly and stops early
-    while step(grid):
+    # Get the probabilities of the neighbours catching fire based on the wind direction and speed
+    wind_probabilities = get_wind_probabilities(args.windspeed, args.winddirection)
+
+    # Run the simulation and save the images for the animation
+    while step(grid, wind_probabilities):
         images.append(return_image(grid))
     ani = animation.ArtistAnimation(fig, images, interval=100, blit=True, repeat_delay=10000)
-
-    # TODO: the gif is not saved properly
     ani.save(f'n={args.size}_p={int(100*args.treeprobability)}%_{args.distribution}.gif', dpi=80, writer='pillow') 
 
     plt.show()
