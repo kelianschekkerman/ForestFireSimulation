34a6ab62aefadac6d9672cf41f401f36034e11ad
meerrom <meerrom@gmail.com>
2024-11-03 13:23:06 +0100

    Create graphs for both ordered and random distributions

--- simulation.py
+++ simulation.py
@@ -1,5 +1,5 @@
 import numpy as np
-import noise
+from noise import pnoise2
 import argparse
 import random
 import matplotlib.pyplot as plt
@@ -29,7 +29,7 @@ def create_arg_parser():
     parser.add_argument("-wd", "--winddirection", default=None, type=str,
                         help="Direction of the wind. Options are: N, S, E, W, NE, NW, SE, SW. Default is no wind.")
     parser.add_argument("-ws", "--windspeed", default=0.0, type=float,
-                        help="The speed of the wind. Default is 0.0, thus no wind. Can range from 0.0 to 0.3.")
+                        help="The speed of the wind. Default is 0.0, thus no wind. Effective range is from 0.0 to 0.5.")
     parser.add_argument("-w", "--weather", default='normal', type=str,
                         help="The weather conditions. Options are: dry, normal, wet. Default is normal.")
     args = parser.parse_args() 
@@ -37,11 +37,11 @@ def create_arg_parser():
 
 # Return the values of the weather conditions
 def get_weather_values(weather):
-    if weather == 'dry':
+    if weather == 'dry':   # trees are more likely to catch fire
         return 1.25
     elif weather == 'normal':
         return 1
-    elif weather == 'wet':
+    elif weather == 'wet': # trees are less likely to catch fire
         return 0.75
 
 # Return the probabilities of neighbors catching fire based on the wind direction and wind speed
@@ -63,10 +63,18 @@ def get_wind_probabilities(args):
     }
     
     # Retrieve probabilities and wind adjustments for the given direction
-    probabilities, ws = direction_map.get(args.winddirection)
+    if wind_speed == 0.0:
+        probabilities, ws = direction_map.get(None)
+    else:
+        probabilities, ws = direction_map.get(args.winddirection)
     # Adjust probabilities by weather and add wind effects
     weather_probabilities = [p * weather for p in probabilities]
     return [sum(x) for x in zip(ws, weather_probabilities)]
+
+# Map a value from one range to another
+def map_value(x, old_min=0, old_max=1, new_min=2, new_max=50):
+    mapped_value = new_min - ((x - old_min) / (old_max - old_min)) * (new_max - new_min)
+    return int(mapped_value)
 
 # Initialize the square lattice with trees based on the distribution type
 def init_square_lattice(args, p):
@@ -77,24 +85,29 @@ def init_square_lattice(args, p):
     if type == 'random': 
         lattice = np.random.choice([0, 1], size=(n, n), p=[1-p, p])
 
-    # TODO: implement variety with p on the ordered lattice
-    # (possibly add scatter so the trees can jump between lines)
-    # vertical tree lines on the lattice
+    # ordered lines on the lattice
     elif type == 'ordered': 
+        d = map_value(p) # distance between lines based on tree density
+        if d < 2:
+            d = 2 # slightly hardcoded to prevent division by zero
+        
         lattice = np.zeros((n, n))
         for i in range(n):
+            if i % d == 0:
+                lattice[i, :] = 1
             for j in range(n):
-                if j % 2 == 0:
-                    lattice[i, j] = 1
+                if j % d == 0:
+                    lattice[:, j] = 1
 
-    # TODO: implement variety with p on the semi-ordered lattice
     # random tree distribution based on perlin noise
     elif type == 'semi-random': 
         lattice = np.zeros((n, n))
+        scale = 1.9 
         for i in range(n):
             for j in range(n):
-                result = noise.pnoise2(i/n, j/n, octaves=10, persistence=1, lacunarity=20.0)
-                lattice[i, j] = 0 if result < 0 else 1
+                 noise_value = pnoise2(i / scale, j / scale)
+                 normalized_value = (noise_value + 1) / 2
+                 lattice[i, j] = 1 if normalized_value < p else 0
 
     return lattice
 
@@ -122,7 +135,11 @@ def run_single_simulation(wind_probabilities, args):
     while step(grid, wind_probabilities):
         images.append(return_image(grid))
     ani = animation.ArtistAnimation(fig, images, interval=100, blit=True, repeat_delay=10000)
-    ani.save(f'gifs\{args.size}_{args.treeprobability}_{args.distribution}_{args.windspeed}_{args.winddirection}_{args.weather}_{args.center}.gif', dpi=80, writer='pillow') 
+
+    if args.distribution == 'random':
+        ani.save(f'gifs\{args.size}_{args.distribution}_{args.treeprobability}_{args.winddirection}_{args.windspeed}_{args.weather}_{args.center}.gif', dpi=80, writer='pillow') 
+    else:
+        ani.save(f'gifs\{args.size}_{args.distribution}_{args.winddirection}_{args.windspeed}_{args.weather}_{args.center}.gif', dpi=80, writer='pillow')
     plt.close(fig)
 
 # Run a single animation based on the given arguments
@@ -171,11 +188,9 @@ def arson(grid, center):
     if center == 'center':
         if grid[n//2, n//2] == 1:
             grid[n//2, n//2] = 2
-            return  
         else:
             closest_tree = find_closest_tree(grid)
             grid[closest_tree[0], closest_tree[1]] = 2
-            return
     elif center == 'random':
         indices = np.argwhere(grid == 1)
         random_index = np.random.choice(indices.shape[0])
@@ -237,38 +252,41 @@ def sigmoid(x, max_val, x0, steepness, y_intercept, ):
     return max_val / (1 + np.exp(-steepness * (x - x0))) + y_intercept
 
 # Create a graph with the data points and the fitted sigmoid curve
-def add_to_graph(values, threshold, variable, color):
+def add_to_graph(values, scatter, threshold, variable, color):
 
     # Define the x and y values of the data and save in numpy arrays
     x_values = np.array([i / len(values) for i in range(len(values))])
     y_values = np.array(values)
 
-    # Fit the sigmoid curve to find the optimal values for our data
-    opt_val, _ = curve_fit(sigmoid, x_values, y_values, maxfev=10000)
+    if scatter:
+        plt.scatter(x_values, y_values, s=20, c=color, label=f"{variable}")
 
-    # Extract the percolation threshold (x0 parameter in the sigmoid function)
-    percolation_threshold = opt_val[1]
+    else:
+        # Fit the sigmoid curve to find the optimal values for our data
+        opt_val, _ = curve_fit(sigmoid, x_values, y_values, maxfev=10000)
 
-    # Calculate smooth x-values for the sigmoid curve
-    x_smooth = np.linspace(min(x_values), max(x_values), 500)
-    y_smooth = sigmoid(x_smooth, *opt_val)
+        # Extract the percolation threshold (x0 parameter in the sigmoid function)
+        percolation_threshold = opt_val[1]
 
-    # Plot the original data and the fitted sigmoid curve
-    plt.plot(x_smooth, y_smooth, c=color, label=f"{variable}")
-    # plt.scatter(x_values, y_values, s=20, c=color) # Uncomment to show data points
+        # Calculate smooth x-values for the sigmoid curve
+        x_smooth = np.linspace(min(x_values), max(x_values), 500)
+        y_smooth = sigmoid(x_smooth, *opt_val)
 
-    if threshold:
-        # Add a vertical line for the percolation threshold
-        plt.axvline(x=percolation_threshold, linestyle='--', c=color, label=f'Percolation threshold ≈ {percolation_threshold:.3f}')
+        # Plot the original data and the fitted sigmoid curve
+        plt.plot(x_smooth, y_smooth, c=color, label=f"{variable}")
+
+        if threshold:
+            # Add a vertical line for the percolation threshold
+            plt.axvline(x=percolation_threshold, linestyle='--', c=color, label=f'Percolation threshold ≈ {percolation_threshold:.3f}')
     
 # Save the graph
-def save_graph(ylabel, title, sub_title, setting):
+def save_graph(ylabel, title, sub_title, setting, distribution):
     plt.xlabel('Tree density')
     plt.ylabel(ylabel)
     plt.title(title + f' and {setting}')
     plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
     plt.tight_layout()
-    plt.savefig(f'figures/{sub_title}_{setting}.png')
+    plt.savefig(f'figures/{sub_title}_{distribution}_{setting}.png')
     plt.close()
 
 # Reset the default values for the given setting
@@ -298,9 +316,10 @@ if __name__ == "__main__":
     wind_speeds = [0.0, 0.1, 0.2, 0.3]
     weather_conditions = ['dry', 'normal', 'wet']
     location = ['center', 'random']
+    distributions = ['random', 'ordered']
     probability_step = 1.00000/max_iter
 
-    settings = [wind_directions, wind_speeds, weather_conditions, location]
+    settings = [wind_speeds, wind_directions, weather_conditions, location]
     colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'orange', 'purple']
 
     wind_probabilities = get_wind_probabilities(args)
@@ -315,44 +334,56 @@ if __name__ == "__main__":
     defaults = {
         "wind_directions":    {'windspeed': 0.1, 'weather': 'normal', 'center': 'center'},
         "wind_speeds":        {'winddirection': 'N', 'weather': 'normal', 'center': 'center'},
-        "weather_conditions": {'winddirection': None, 'windspeed': 0.1, 'center': 'center'},
-        "location":           {'winddirection': None, 'windspeed': 0.1, 'weather': 'normal'}
+        "weather_conditions": {'winddirection': None, 'windspeed': 0.0, 'center': 'center'},
+        "location":           {'winddirection': None, 'windspeed': 0.0, 'weather': 'normal'}
     }
 
-    # Loop through all settings and variables to generate graphs
-    for setting in tqdm(settings):
-        burnt_percentages = []
-        percolating_percentages = []
-        setting_name = "wind_directions" if setting == wind_directions else \
-                       "wind_speeds" if setting == wind_speeds else \
-                       "weather_conditions" if setting == weather_conditions else \
-                       "location"
-        reset_defaults(setting_name)
+    # Loop through all distributions and settings to generate graphs
+    for distribution in tqdm(distributions, desc='Distributions'):
+        args.distribution = distribution
 
-        # Loop through all variables for the given setting
-        for variable in tqdm(setting):
-            wind_probabilities = update_args_for_variable(setting_name, variable)
-            treeprobability = probability_step
-            burnt_percentage = []
-            percolating_percentage = []
-            
-            # Run main simulation loop and keep track of the burnt and percolating percentages
-            for i in tqdm(range(max_iter - 1)):
-                percolating_amount, burnt_trees_amount = run_simulation(args, wind_probabilities, treeprobability)
-                burnt_percentage.append(burnt_trees_amount / max_subiter)
-                percolating_percentage.append(percolating_amount / max_subiter)
-                treeprobability += probability_step
+        # Loop through all settings and variables to generate graphs
+        for setting in tqdm(settings, desc='Settings'):
+            burnt_percentages = []
+            percolating_percentages = []
+            setting_name =  "wind_directions" if setting == wind_directions else \
+                            "wind_speeds" if setting == wind_speeds else \
+                            "weather_conditions" if setting == weather_conditions else \
+                            "location"
+            reset_defaults(setting_name)
 
-            percolating_percentages.append(percolating_percentage)
-            burnt_percentages.append(burnt_percentage)
+            # Loop through all variables for the given setting
+            for variable in tqdm(setting, desc=setting_name):
+                wind_probabilities = update_args_for_variable(setting_name, variable)
+                treeprobability = probability_step
+                burnt_percentage = []
+                percolating_percentage = []
+                
+                # Run main simulation loop and keep track of the burnt and percolating percentages
+                for i in tqdm(range(max_iter - 1), desc='Run the simulation'):
+                    percolating_amount, burnt_trees_amount = run_simulation(args, wind_probabilities, treeprobability)
+                    burnt_percentage.append(burnt_trees_amount / max_subiter)
+                    percolating_percentage.append(percolating_amount / max_subiter)
+                    treeprobability += probability_step
 
-        # Generate graphs
-        plt.figure(figsize=(12, 6))
-        for idx, percolating_percentage in enumerate(percolating_percentages):
-            add_to_graph(percolating_percentage, True, setting[idx], colors[idx])
-        save_graph('Percolating percentage', 'Percolating percentage based on tree density', 'p', setting_name)
+                percolating_percentages.append(percolating_percentage)
+                burnt_percentages.append(burnt_percentage)
 
-        plt.figure(figsize=(12, 6))
-        for idx, burnt_percentage in enumerate(burnt_percentages):
-            add_to_graph(burnt_percentage, False, setting[idx], colors[idx])
-        save_graph('Burnt trees percentage', 'Burnt trees percentage based on tree density', 'b', setting_name)
+            # Generate both the percolating and burnt trees graphs
+            if distribution == 'random':
+                plt.figure(figsize=(12, 6))
+                for idx, percolating_percentage in enumerate(percolating_percentages):
+                    add_to_graph(percolating_percentage, False, True, setting[idx], colors[idx])
+                save_graph('Percolating percentage', 'Percolating percentage based on tree density', 'p', setting_name, distribution)
+
+                plt.figure(figsize=(12, 6))
+                for idx, burnt_percentage in enumerate(burnt_percentages):
+                    add_to_graph(burnt_percentage, False, False, setting[idx], colors[idx])
+                save_graph('Burnt trees percentage', 'Burnt trees percentage based on tree density', 'b', setting_name, distribution)
+
+            # Generate only the burnt trees graph
+            else:
+                plt.figure(figsize=(12, 6))
+                for idx, burnt_percentage in enumerate(burnt_percentages):
+                    add_to_graph(burnt_percentage, True, False, setting[idx], colors[idx])
+                save_graph('Burnt trees percentage', 'Burnt trees percentage based on tree density', 'b', setting_name, distribution)
